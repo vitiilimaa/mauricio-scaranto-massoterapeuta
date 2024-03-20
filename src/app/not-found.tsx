9f136c72b343d9db1a8a2f893f4eaaf94cb11e5f
Vitor Batista <vitiilimaa@gmail.com>
2024-03-19 23:47:01 -0300

    Ajuste de espaçamento nos componentes na tela de erro

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 "use client";
 import CustomButton from "@luzDeShiva/components/CustomButton";
+import MainContainer from "@luzDeShiva/components/MainContainer";
 import SectionTitle from "@luzDeShiva/components/SectionTitle";
 import { useRouter } from "next/navigation";
 
@@ -7,17 +8,21 @@ const Error = () => {
   const router = useRouter();
 
   return (
-    <div className="min-h-screen flex flex-column justify-content-center align-items-center px-5">
+    <MainContainer addClass="pb-0">
       <SectionTitle title="404" addClassContainer="absolute top-0" />
-      <p className="text-center text-4xl sm:text-5xl">
-        A PÁGINA QUE VOCÊ TENTOU ACESSAR NÃO FOI ENCONTRADA ;(
-      </p>
-      <CustomButton
-        title="VOLTAR PARA O INÍCIO"
-        addClass="text-xl sm:text-2xl"
-        handle={() => router.push('/')}
-      />
-    </div>
+      <div className="flex flex-column text-center m-auto">
+        <p className="mt-8 text-4xl sm:text-5xl">
+          A PÁGINA QUE VOCÊ TENTOU ACESSAR NÃO FOI ENCONTRADA ;(
+        </p>
+        <div className="text-center">
+          <CustomButton
+            title="VOLTAR PARA O INÍCIO"
+            addClass="text-xl sm:text-2xl"
+            handle={() => router.push("/")}
+          />
+        </div>
+      </div>
+    </MainContainer>
   );
 };
 
